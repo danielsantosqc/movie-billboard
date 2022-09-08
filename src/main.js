a62e06bbaacf8855f7092c2df240e24077edacd2
Daniel Santos <danielsantosqc.dev@gmail.com>
2022-09-08 18:57:56 -0300

    add new functions for get information to movie genres/ adicione nueva funcion para requsitar informacion de los generos de peliculas y mostrarlas en ola pagina

--- src/main.js
+++ src/main.js
@@ -1,38 +1,65 @@
-console.log(API_KEY);
-const ULR_API =
+const ULR_API_TRENDING =
   "https://api.themoviedb.org/3/trending/movie/day?api_key=" + API_KEY;
-
-console.log(ULR_API);
+const ULR_API_GENRES =
+  "https://api.themoviedb.org/3/genre/movie/list?api_key=" + API_KEY;
 
 async function getTrendingMoviesPreview() {
-  try {
-    const res = await fetch(ULR_API);
-    const data = await res.json();
-    if ((res.status = 200)) {
-      const trendingPreviweMoviesContainer = document.querySelector(
-        "#trendingPreview .trendingPreview-movieList"
-      );
-      console.log("Biesssn");
-      console.log(data);
-      data.results.forEach((movie) => {
-        const movieContainer = document.createElement("div");
-        movieContainer.classList.add("movie-container");
+  const res = await fetch(ULR_API_TRENDING);
+  const data = await res.json();
+  if ((res.status = 200)) {
+    console.log("Bien");
+    console.log(data);
 
-        const movieImg = document.createElement("img");
-        movieImg.classList.add("movie-img");
-        movieImg.setAttribute("alt", movie.title);
-        movieImg.setAttribute(
-          "src",
-          "https://image.tmdb.org/t/p/w300" + movie.poster_path
-        );
-        movieContainer.appendChild(movieImg);
-        trendingPreviweMoviesContainer.appendChild(movieContainer);
-      });
-    } else {
-      console.log("error");
-    }
-  } catch (error) {
-    console.log(error);
+    const trendingPreviweMoviesContainer = document.querySelector(
+      "#trendingPreview .trendingPreview-movieList"
+    );
+
+    data.results.forEach((movie) => {
+      const movieContainer = document.createElement("div");
+      movieContainer.classList.add("movie-container");
+
+      const movieImg = document.createElement("img");
+      movieImg.classList.add("movie-img");
+      movieImg.setAttribute("alt", movie.title);
+      movieImg.setAttribute(
+        "src",
+        "https://image.tmdb.org/t/p/w300" + movie.poster_path
+      );
+      movieContainer.appendChild(movieImg);
+      trendingPreviweMoviesContainer.appendChild(movieContainer);
+    });
+  } else {
+    console.log("error");
   }
 }
+
+async function getGenresMoviesPreview() {
+  const res = await fetch(ULR_API_GENRES);
+  const data = await res.json();
+  if (res.status == 200) {
+    console.log("ok ");
+    console.log(data);
+
+    const previewCategorieseContainer = document.querySelector(
+      "#categoriesPreview .categoriesPreview-list"
+    );
+    
+    data.genres.forEach((genre) => {
+      
+      const categoryContainer = document.createElement("div");
+      categoryContainer.classList.add("category-container")
+
+      const categoryTitle = document.createElement("h3");
+      categoryTitle.classList.add("category-title")
+      categoryTitle.setAttribute("id", "id"+genre.id);
+      categoryTitle.textContent = genre.name;
+      categoryContainer.appendChild(categoryTitle);
+      previewCategorieseContainer.appendChild(categoryContainer);
+    });
+  } else {
+    console.log("algo salio mal");
+  }
+}
+
 getTrendingMoviesPreview();
+getGenresMoviesPreview();
